3790eec7be8f6608d948c8d43faba42a1c0f3b9e
Albert Yakubov <mrayakubov@hotmail.com>
2019-09-10 17:21:49 -0600

    Travel finished Project
    pop up fragment works but details still dont display
    
    Co-Authored-By: Basil <heiligbasil@users.noreply.github.com>

--- M02/Assignment/app/src/main/java/com/example/travel/ItemDetailActivity.kt
+++ M02/Assignment/app/src/main/java/com/example/travel/ItemDetailActivity.kt
@@ -69,4 +69,5 @@ class ItemDetailActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+
 }
--- M02/Assignment/app/src/main/java/com/example/travel/ItemDetailFragment.kt
+++ M02/Assignment/app/src/main/java/com/example/travel/ItemDetailFragment.kt
@@ -1,11 +1,13 @@
 package com.example.travel
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.travel.dummy.DummyContent
+import com.example.travel.model.Vehicle
 import kotlinx.android.synthetic.main.activity_item_detail.*
 import kotlinx.android.synthetic.main.item_detail.view.*
 
@@ -20,7 +22,7 @@ class ItemDetailFragment : Fragment() {
     /**
      * The dummy content this fragment is presenting.
      */
-    private var item: DummyContent.DummyItem? = null
+    private var item: Vehicle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +32,8 @@ class ItemDetailFragment : Fragment() {
                 // Load the dummy content specified by the fragment
                 // arguments. In a real-world scenario, use a Loader
                 // to load content from a content provider.
-                item = DummyContent.ITEM_MAP[it.getString(ARG_ITEM_ID)]
-                activity?.toolbar_layout?.title = item?.content
+
+                activity?.toolbar_layout?.title = item?.travelBy()
             }
         }
     }
@@ -44,10 +46,27 @@ class ItemDetailFragment : Fragment() {
 
         // Show the dummy content as text in a TextView.
         item?.let {
-            rootView.item_detail.text = it.details
+            rootView.item_detail.text = it.travelBy()
         }
 
         return rootView
+    }
+    interface DetailResponse{
+        fun provideInfoForObject(info: String)
+    }
+    private var objectResponse: DetailResponse? = null
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+    if (context is DetailResponse){
+        objectResponse = context
+    }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        objectResponse = null
+
     }
 
     companion object {
@@ -57,4 +76,5 @@ class ItemDetailFragment : Fragment() {
          */
         const val ARG_ITEM_ID = "item_id"
     }
+
 }
