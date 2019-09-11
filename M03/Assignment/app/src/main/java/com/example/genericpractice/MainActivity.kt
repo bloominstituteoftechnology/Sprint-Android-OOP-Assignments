c55ffc715d2c8350702692d07cf693e5191925ac
Albert Yakubov <mrayakubov@hotmail.com>
2019-09-11 14:07:53 -0600

    StringList
    update stringList to show in text view
    
    Co-Authored-By: Basil <heiligbasil@users.noreply.github.com>

--- M03/Assignment/app/src/main/java/com/example/genericpractice/MainActivity.kt
+++ M03/Assignment/app/src/main/java/com/example/genericpractice/MainActivity.kt
@@ -2,24 +2,36 @@ package com.example.genericpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
     }
 
     override fun onStart() {
         super.onStart()
         // TODO 1 Create a simple example of a mutableList that takes strings as parameters
-        val myStringList = singleList(1)
+       val myStringList = singleList("MY STRING LIST" )
+
+    textView.setText("this is my single list: $myStringList")
+
     }
 
     fun <T> singleList(t: T): List<T> {
         val theList = mutableListOf<T>()
+        theList.add(t)
         return theList
     }
+
+
 }
 
 
@@ -30,5 +42,7 @@ class Phone1
         fun fetch(): T? {
             return item.takeIf { open }
         }
+
     }
+
 
