cd72acfb4fb29ff08391a57b75394ec45f39a575
Albert Yakubov <mrayakubov@hotmail.com>
2019-09-11 17:43:58 -0600

    GenericsPractice
    Step 9 testing
    unable to populate the taxt view with dummy data, returns as code
    
    Co-Authored-By: Basil <heiligbasil@users.noreply.github.com>

--- M03/Assignment/app/src/main/java/com/example/genericpractice/GenericActivity.kt
+++ M03/Assignment/app/src/main/java/com/example/genericpractice/GenericActivity.kt
@@ -13,13 +13,69 @@ class GenericActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generic)
 
-    val observer = Observable.just("Tom Cruise",
-        "Brat Pitt", "Jeniffer Aniston")
+        val observer = Observable.just(
+            "Tom Cruise",
+            "Brat Pitt", "Jeniffer Aniston"
+        )
 
-        observer.subscribe{ _ ->
+        observer.subscribe { _ ->
 
-            textView2.text = "$observer"
+            textView2.text = ""
         }
 
 
-}}
+        data class Movie(val releaseYear: String, val title: String, val language: String)
+
+
+        class FakeAPI {
+
+
+
+    fun createMovieList(): Observable<List<Movie>> {
+
+        return Observable.just(
+            listOf(
+
+                Movie(
+                    "1992",
+
+                    "The Mighty Ducks",
+
+                    "English"
+                ),
+
+                Movie(
+                    "2011",
+
+                    "Moneyball",
+
+                    "English"
+                ),
+
+                Movie(
+                    "1965",
+
+                    "The Saragossa Manuscript",
+
+                    "Polish"
+                ),
+
+                Movie(
+                    "1994",
+
+                    "Pulp Fiction",
+
+                    "English"
+                )
+
+            )
+        )
+
+    }
+
+}
+
+            }
+
+        }
+
