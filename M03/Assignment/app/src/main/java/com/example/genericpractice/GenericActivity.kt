e2a3af14b0431ae0d2c2ac1b9bcc530d15e9dd23
Albert Yakubov <mrayakubov@hotmail.com>
2019-09-11 14:14:44 -0600

    Update GenericActivity.kt
    Co-Authored-By: Basil <heiligbasil@users.noreply.github.com>

--- M03/Assignment/app/src/main/java/com/example/genericpractice/GenericActivity.kt
+++ M03/Assignment/app/src/main/java/com/example/genericpractice/GenericActivity.kt
@@ -9,29 +9,4 @@ class GenericActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generic)
     }
-
-    override fun onStart() {
-        super.onStart()
-
-        open class Ball
-
-
-        class Volleyball: Ball()
-        open class GolfBall: Ball()
-        class SomeBall : GolfBall()
-
-        class Box<T: Ball>(private var item: Ball)
-
-        val ball = Ball()
-        val volleyball = Volleyball()
-        val golfball = GolfBall()
-        val someball = SomeBall()
-
-
-        val ballBox = Box<Ball>(ball)
-        val volleyBox = Box<Ball>(volleyball)
-        val golfBallBox = Box<Ball>(golfball)
-        val someBallBox = Box<Ball>(someball)
-    }
-
 }
