8941a340a180a6b42fbc28605a728817bec638bc
Albert Yakubov <mrayakubov@hotmail.com>
2019-09-11 15:00:11 -0600

    GenericPractice(observer)
    add an observer
    
    Co-Authored-By: Basil <heiligbasil@users.noreply.github.com>

--- M03/Assignment/app/src/main/java/com/example/genericpractice/GenericActivity.kt
+++ M03/Assignment/app/src/main/java/com/example/genericpractice/GenericActivity.kt
@@ -1,12 +1,25 @@
 package com.example.genericpractice
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.Observable
+import kotlinx.android.synthetic.main.activity_generic.*
 
 class GenericActivity : AppCompatActivity() {
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generic)
-    }
-}
+
+    val observer = Observable.just("Tom Cruise",
+        "Brat Pitt", "Jeniffer Aniston")
+
+        observer.subscribe{ _ ->
+
+            textView2.text = "$observer"
+        }
+
+
+}}
